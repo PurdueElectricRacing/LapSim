a2e69d0798fcf63c8fc54504fd808dee3ed2b7d7
Zachary Ellis <48958615+zaellis@users.noreply.github.com>
2020-06-30 21:58:19 -0400

    Filled in 75% of battery data. Parser script is modified to get values for charging HPPC at the moment

--- lapsim/battery_data_parser.m
+++ lapsim/battery_data_parser.m
@@ -5,18 +5,18 @@
 % OCV(1:2:end) = [];
 % xlswrite('VTC6_Data.xlsx', [SOC,OCV], 1, 'C4');
 % plot(SOC,OCV);
-v = xlsread('VTC6 RPT.csv', 'I50665:I51386') / 1000;
-t = xlsread('VTC6 RPT.csv', 'D50666:D51386');
+v = xlsread('VTC6 RPT.csv', 'I67878:I68239') / 1000;
+t = xlsread('VTC6 RPT.csv', 'D67879:D68239');
 t = t - t(1);
 SOC_C = interp1(OCV, SOC, v(end));
-sr = (v(2) - v(1)) / 3;
+sr = (v(1) - v(2)) / 0.9; %charging mode
 v = v(2:end);
 dif = (max(v) - min(v));
-r = dif / 3;
-c = 1000:20000;
+r = dif / 0.9;
+c = 100:20000;
 corr = 0;
 for i = c(1):c(end)
-    x = max(v) - (dif * exp(-t / (r * i)));
+    x = min(v) + (dif * exp(-t / (r * i))); %charging mode
     R = abs(x - v) ./ v;
     R = mean(R) * 100;
     R = 1 - R;
