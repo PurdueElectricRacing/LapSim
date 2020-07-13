b9ae24eceae05c4b0c9df0830e77b8b4752dbaf9
Dawson Moore <moore800@purdue.edu>
2020-07-13 18:01:44 -0500

    Added comments to HV variables
    Some are still unknown since I wasn't at the meeting brainstorming variable names

--- lapsim/classes/hv.m
+++ lapsim/classes/hv.m
@@ -1,24 +1,24 @@
 classdef hv
     properties
         % Inputs
-        torqueCommand
-        coolingPower
-        rpm_in
+        torqueCommand       % Current commanded torque in N * m
+        coolingPower        % Cooling power in W
+        rpm_in              % Current motor rate in rpm
         
         % Internal values
-        cable_r
-        cable_z
-        DC_link_cap
-        eff_map
-        cable_voltage_drop % Seems repeated given we know motor current and cabling resistance
-        cable_energy_loss  % Again, same thing
-        heatRej
-        invPowerDraw
+        cable_r             % Effective resistance of HV cabling in ohms
+        cable_z             % Effective impedance of HV cabling in ohms
+        DC_link_cap         % Don't really know what we were shooting for on this one
+        eff_map             % Same here
+        cable_voltage_drop  % Seems repeated given we know motor current and cabling resistance
+        cable_energy_loss   % Again, same thing
+        heatRej             % Also don't know here
+        invPowerDraw        % Power draw of inverter in W
         
         % Outputs
-        motorCurrent
-        rpm_out
-        efficiency
+        motorCurrent        % Current going to motor in A
+        rpm_out             % Output rate due to command in rpm
+        efficiency          % Overall HV system efficiency as a ratio (0-1)
     end
     
     methods
