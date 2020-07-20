5906420a2129f2013bbe0e97cc4017d5babf49dd
Dawson Moore <moore800@purdue.edu>
2020-07-20 01:30:36 -0500

    Add pulled files in *correct* location this time

--- lapsim/classes/battery.m
+++ lapsim/classes/battery.m
@@ -15,6 +15,10 @@ classdef battery
         cell_OCVvsTime
         cell_r1
         cell_c1
+        batt_voltage
+        res_eff
+        batt_power
+        batt_current_limit
         
         %Outputs
         ActualCurrent
@@ -23,6 +27,8 @@ classdef battery
         temperature_cell
         mass_batteryPack
         centerMass_batteryPack
+        capacity
+        pack_SOC
     end
     
     methods
@@ -34,6 +40,14 @@ classdef battery
                 obj.mass_batteryPack = mass;
                 obj.centerMass_batteryPack = com;
             end
+        end
+
+        function computeState(self, time_step)
+            % This function calculates battery capacity, SOC, and OCV and stores them into the battery class
+            capacity_new = ((self.SOC / 100) * self.capacity) - (self.ActualCurrent * (time_step / 3600)); %new battery amp hours
+            self.SOC = (capacity_new / self.capacity) * 100; %new battery state of charge
+            self.capacity = capacity_new;
+            self.new_OCV = battery_table_interp((uint16(round(self.pack_SOC, 2) * 100) + 1), 2) * self.cell_s; %new battery open circuit voltage
         end
     end
 end
