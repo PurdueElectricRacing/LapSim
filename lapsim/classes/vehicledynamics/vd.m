aae34347495af038ac2844da657aa1632dd2eb90
smithjohn16 <66568779+smithjohn16@users.noreply.github.com>
2020-09-08 14:05:53 -0400

    Update vd.m

--- lapsim/classes/vehicledynamics/vd.m
+++ lapsim/classes/vehicledynamics/vd.m
@@ -1,5 +1,6 @@
 classdef vd < handle
     properties
+        %Inputs
         susp_params
         veh_params
         acceleration
@@ -113,7 +114,7 @@ classdef vd < handle
             %not done
         end
         
-        function [dfr, dfl, drr, drl] = weight_transfer_variable(self)
+        function [fr, fl, rr, rl] = weight_transfer_variable(self,rho)
             %calculates the change in force on each tire due to
             %accelerations
             Cgz = self.veh_params.Cg_xyz(3);
@@ -123,56 +124,72 @@ classdef vd < handle
             mass = self.veh_params.mass;
             
             syms v_max;
-            lat_accel_capacity = v_max^2 / corner;
+            lat_accel_capacity = v_max^2 / rho;
             
-            longitudinal =  self.Accel(1)*mass*Cgz/wheelbase; %positive rear
+            longitudinal =  0; %self.Accel(1)*mass*Cgz/wheelbase; positive rear
             front_lateral = lat_accel_capacity*mass*Cgz/track_front; %positive right
             rear_lateral = lat_accel_capacity*mass*Cgz/track_rear;
             
-            dfr = -longitudinal + front_lateral;
-            dfl = -longitudinal - front_lateral;
-            drr = longitudinal + rear_lateral;
-            drl = longitudinal - rear_lateral;
+            fr = -longitudinal + front_lateral + (.25 * mass);
+            fl = -longitudinal - front_lateral + (.25 * mass);
+            rr = longitudinal + rear_lateral + (.25 * mass);
+            rl = longitudinal - rear_lateral + (.25 * mass);
         end
         
-        function [max_speed_table] = max_corner_speed(track_inputs, tire_coeffs)
+        function [max_speed_table] = max_corner_speed(track_table)
             %determines maximum corner speed for each corner and outputs to
             %table based on track input table
-            i = 1;
-            for track_inputs i=1:length(??track_inputs??)
-                if CORNER VAL = CORNER VAL  %true if line in track table is a corner not a straight
-                    syms v_max;    %variable to solve for
-                    [dfr, dfl, drr, drl] = weight_transfer_variable(self);   %determine weight transfer in terms of v_max
+            
+            tire_coef = 1.5;
+            i=1;
+            for track_inputs i=1:length(track_table)
+                if track_table(i,3) ~= 0                                      %true if line in track table is a corner not a straight
+                    syms v_max;                                             %variable to solve for
+                    rho = track_table(i,3);                                    %corner radius for current corner
                     lat_accel = v_max^2 / rho;                              %expression for lateral acceleration in terms of v_max
-                    self.acceleration = (tire_coeffs * (dfr + dfl + drr +drl)) / self.mass == lat_accel;     %available traction @ v_max == lateral accel @v_max
-                    max_speed_table(i) = vpasolve(accceleration, v_max);        %solve for v_max in this corner and put in max_speed_table
-                    i = i+1;                      %increment index value
-                else  % if line of track table is a straight
-                        max_speed_table(i) = 0;     %placeholder for v_max on straightaway
-                        i = i+1;          %increment index value
+                    [fr, fl, rr, rl] = weight_transfer_variable(self, rho);   %determine weight transfer in terms of v_max
+                    acceleration_eq = (tire_coef * (fr + fl + rr + rl)) / self.mass == lat_accel;     %available traction @ v_max == lateral accel @v_max: this is just an equation that is solved for the max velocity in the next line
+                    max_speed_table(i) = vpasolve(accceleration_eq, v_max);        %solve for v_max in this corner and put in max_speed_table
+                    i=i+1;                                                %increment index value
+                else                                                     %if line of track table is a straight
+                    max_speed_table(i) = 0;                              %placeholder for v_max on straightaway
+                    i=i+1;                                                %increment index value
                 end
             end
         end
             
-        function [motor_torque_used] = vd_main(max_motor_torque, time_step, straight_length)
-            %inputs: max_motor_torque at wheel center for current conditions, system time step, car mass, track current straight length
+  
+        function = vd_main(aero, drivetrain);
+            %inputs: "max_motor_torque" instantanously at wheel center for current conditions in Newton-meters, system time step (seconds), car mass (kg), track table
             %outputs: current velocity, motor_torque_used for one wheel given time step
-            %needs to clear curr_distance and dist_corner when new straight is entered
-
             wheel_radius = 0.2286; %wheel diameter in meters
             coef = 1.5; %tire coefficient of friction
-            num_motor = 2; %number of hub motors 2- rear wheel drive, 4 - all wheel drive
+            num_motor = 4; %%%%%%%%%%%%number of hub motors 2- rear wheel drive, 4 - all wheel drive
+            max_motor_torque = drivetain.max_motor_torque; %%%%%%get input of max instanteous torque
 
-            self.curr_distance = self.curr_distance + (time_step * self.curr_velocity);  %distance counter
-            dist_corner = self.curr_distance - straight_length; %calc distance to corner with straight length and current distance
+            radius = track_table(k,3); %get radius of turn
+            if radius ~= 0
+                dist_corner = 0;
+                curr_velocity = max_speed_table(k);
+                corner = 0; %not in a corner
+            else
+                corner = 1; % in a corner
+            end
+
+            straight_length = track_table(k,2); %pass length of straight
+            dist_corner = track_table(k,3) - (time_step * curr_velocity); %calc distance to corner with straight length and distance change
+            if dist_corner <= 0
+                k = k+1; %increment to next part of track
+            end
+
             low_velocity = sqrt(curr_velocity^2 + (-19.62)*dist_corner); %determine lowest acheivable velocity at start of turn with 2G of braking
 
             RR_force = (.005 + (1/1.379) * (0.01+0.0095*((curr_velocity*1000)/100)^2))*(vehicle_mass*9.81); %calc current rolling resistance
-            drag_force = aero_calc(self, curr_velocity); %call aero "aero_calc" function and get output of self.drag_force
+            drag_force = aero.drag_force; %call aero "aero_calc" function and get output of self.drag_force
 
-            if self.curr_velocity <= max_speed_table(k) && max_speed_table(k) >= low_velocity && dist_corner >0 %checks if car can still accelerate and be able to brake in time
+            if curr_velocity <= max_speed_table(k) && max_speed_table(k) >= low_velocity && corner == false %checks if car can still accelerate and be able to brake in time
                 motor_force = max_motor_torque / wheel_radius; %calc acceleration force of car
-                fric_force = coef * self.vehicle_mass/4; %%%%%needs more accurate normal force
+                fric_force = coef * vehicle_mass/4; %
                 if motor_force < fric_force
                     accel_force = motor_force * num_motor;
                     motor_torque_used = max_motor_torque;
@@ -181,15 +198,14 @@ classdef vd < handle
                     motor_torque_used = fric_force / wheel_radius;
                 end
 
-                self.curr_velocity = (accel_force - RR_force - drag_force)/vehicle_mass * time_step; %accelerate car
+                curr_velocity = curr_velocity +(((accel_force*num_motor) - RR_force - drag_force)/vehicle_mass * time_step); %accelerate car
 
-            elseif self.curr_velocity > max_speed_table(k) && dist_corner >0 %checks if car needs to brake
-                self.curr_velocity = self.curr_velocity - (19.62*time_step); %brakes car at -2G
+            elseif curr_velocity > max_speed_table(k) && corner == false %checks if car needs to brake
+                curr_velocity = curr_velocity + (curr_velocity - (19.62*time_step)); %brakes car at -2G
 
-            else %checks if car should stay going same velocity b/c its in the turn
+            else %checks if car should stay going same velocity b/c its in a turn
                 motor_torque_used = (RR_force + drag_force) / num_motor * wheel_radius; %overcome rolling resistance and drag
             end
         end
-    end
 end
 
